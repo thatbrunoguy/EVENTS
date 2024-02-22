5012ab027bd57f6d0273147029dacb552ab6c252
Christian Peters <auraqule@gmail.com>
2024-02-22 12:53:55 +0100

    feat/reduced hero height on mobile

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -95,7 +95,7 @@ const Home = () => {
     <section>
       {/* HERO */}
       <div className="w-[98%] mx-auto">
-        <div className="relative h-[50vh] md:h-[56vh] mt-6">
+        <div className="relative h-[46vh] md:h-[56vh] mt-1 md:mt-6">
           <div className="h-full relative w-full rounded-lg overflow-hidden ">
             <Image
               className="object-cover"
